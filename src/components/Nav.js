bf8156d179739c6694b76b80c734d1d96c27eeae
Sinista121 <mohammedsaajith93@gmail.com>
2024-03-10 18:46:26 +0400

    small update (home page and nav)

---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+export default function Nav() {
+  return (
+    <nav className="flex items-center justify-center gap-5 p-5">
+      <a>Dashboard</a>
+      <a>Events</a>
+      <a>Support</a>
+    </nav>
+  );
+}
